6dac95d18acf9695b95b4ada3ff167e5c4db6a29
Kabir Bhardwaj <kabirbhardwaj026@gmail.com>
2026-08-06 23:01:48 +0530

    .

---
+++ Hybrid_Search/HybridSearch.py
@@ -0,0 +1,35 @@
+from langchain_core.documents import Document
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
+from langchain_community.retrievers import BM25Retriever
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+documents = [Document(
+    page_content = "EMP001 works in AI department"
+),
+    Document(
+    page_content = "EMP002 works in HR department , lives in delhi "
+),
+    Document(
+        page_content = "EMP003 works in Health department"
+)]
+# for doc in documents:
+#     print(doc.page_content)
+
+embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-MiniLM-L6-v2"
+            )
+vector_db = FAISS.from_documents(
+    documents,
+    embeddings
+)
+
+vector_retriever = vector_db.as_retriever()
+
+query = "where EMP002 lives "
+
+result = vector_retriever.invoke(query)
+print(result)
